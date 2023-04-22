1042cd753e4739ab397b354e72824d0cf7c3a8f8
Taeho_Kim <teahotiger@gmail.com>
2023-04-23 01:40:35 +0900

    구현 URL 수정

--- HTML/stock_crawling.py
+++ HTML/stock_crawling.py
@@ -2,9 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 # URL = https://kr.investing.com/equities/south-korea 종목 : KOSPI 100
-header = {'User-agent' : 'Mozila/2.0'}
-# 주식 거래 정보: https://api.investing.com/api/financialdata/assets/equitiesByIndices/49660?fields-list=id%2Cname%2Csymbol%2Chigh%2Clow%2Clast%2ClastPairDecimal%2Cchange%2CchangePercent%2Cvolume%2Ctime%2CisOpen%2Curl%2Cflag%2CcountryNameTranslated%2CexchangeId%2CperformanceDay%2CperformanceWeek%2CperformanceMonth%2CperformanceYtd%2CperformanceYear%2Cperformance3Year%2CtechnicalHour%2CtechnicalDay%2CtechnicalWeek%2CtechnicalMonth%2CavgVolume%2CfundamentalMarketCap%2CfundamentalRevenue%2CfundamentalRatio%2CfundamentalBeta&country-id=&page=0&page-size=0&include-major-indices=false&include-additional-indices=false&include-primary-sectors=false&include-other-indices=false&limit=0
-response_URL = requests.get("https://kr.investing.com/equities/south-korea", headers=header)
+response_URL = requests.get("https://api.investing.com/api/financialdata/assets/equitiesByIndices/49660?fields-list=id%2Cname%2Csymbol%2Chigh%2Clow%2Clast%2ClastPairDecimal%2Cchange%2CchangePercent%2Cvolume%2Ctime%2CisOpen%2Curl%2Cflag%2CcountryNameTranslated%2CexchangeId%2CperformanceDay%2CperformanceWeek%2CperformanceMonth%2CperformanceYtd%2CperformanceYear%2Cperformance3Year%2CtechnicalHour%2CtechnicalDay%2CtechnicalWeek%2CtechnicalMonth%2CavgVolume%2CfundamentalMarketCap%2CfundamentalRevenue%2CfundamentalRatio%2CfundamentalBeta&country-id=&page=0&page-size=0&include-major-indices=false&include-additional-indices=false&include-primary-sectors=false&include-other-indices=false&limit=0")
 
 html = response_URL.text
 print(html)
