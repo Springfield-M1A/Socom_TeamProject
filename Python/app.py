944be7aaf9cc0169621ef82c6139949e485b80f3
M1A_ <ayd1fxmx@gmail.com>
2023-05-23 18:45:44 +0900

    .

--- Python/app.py
+++ Python/app.py
@@ -1,33 +1,22 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('HTML/index.html')
+    return render_template('index.html')
 
-@app.route('/')
+@app.route('/stock')
 def stock():
-    return render_template('HTML/stock.html')
+    return render_template('stock.html')
 
-@app.route('/')
+@app.route('/prediction')
 def prediction():
-    return render_template('HTML/prediction.html')
+    return render_template('prediction.html')
 
-@app.route('/')
+@app.route('/beginner')
 def beginner():
-    return render_template('HTML/beginner.html')
-
-@app.route('/install_requirements', methods=['POST'])
-def install_requirements():
-    try:
-        result = subprocess.run(["pip", "install", "-r", "requirements.txt"], capture_output=True, text=True)
-        if result.returncode == 0:
-            return jsonify({'message': 'Requirements 설치가 완료되었습니다.'})
-        else:
-            return jsonify({'message': 'Requirements 설치 중 오류가 발생하였습니다.'})
-    except Exception as e:
-        return jsonify({'message': 'Requirements 설치 중 오류가 발생하였습니다.'})
+    return render_template('beginner.html')
 
 if __name__ == '__main__':
     app.run()
