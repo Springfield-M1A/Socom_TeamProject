60dd0c9d5aa35d9cb990d9ac9e176893ea6deec4
M1A_ <ayd1fxmx@gmail.com>
2023-05-23 18:47:59 +0900

    Update app.py

--- Python/app.py
+++ Python/app.py
@@ -4,19 +4,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('HTML/index.html')
 
 @app.route('/stock')
 def stock():
-    return render_template('stock.html')
+    return render_template('HTML/stock.html')
 
 @app.route('/prediction')
 def prediction():
-    return render_template('prediction.html')
+    return render_template('HTML/prediction.html')
 
 @app.route('/beginner')
 def beginner():
-    return render_template('beginner.html')
+    return render_template('HTML/beginner.html')
 
 if __name__ == '__main__':
     app.run()
