cfbc717c0b8ee9301f542cb0dc5c57fc662404c6
M1A_ <ayd1fxmx@gmail.com>
2023-05-24 05:56:52 +0900

    .

--- Socom_TeamProject/views.py
+++ Socom_TeamProject/views.py
@@ -1,3 +1,4 @@
+import os
 from django.shortcuts import render
 
 
@@ -12,49 +13,6 @@ def prediction(request):
 
 def beginner(request):
     return render(request, 'beginner.html')
-
-class Requirement:
-    def __init__(self, name, installed):
-        self.name = name
-        self.installed = installed
-
-def requirements(request):
-    # requirements.txt 파일의 경로
-    requirements_file = os.path.join(os.path.dirname(__file__), 'requirements.txt')
-
-    # requirements.txt 파일을 읽어서 요구 사항 리스트 생성
-    requirements = []
-    with open(requirements_file, 'r') as file:
-        for line in file:
-            requirement_name = line.strip()
-            installed = is_package_installed(requirement_name)  # 패키지 설치 여부 확인
-            requirement = Requirement(name=requirement_name, installed=installed)
-            requirements.append(requirement)
-
-    # POST 요청인 경우, 요구 사항을 설치
-    if request.method == 'POST':
-        for requirement in requirements:
-            if not requirement.installed:
-                install_package(requirement.name)
-
-        # 설치 후 요구 사항을 다시 확인
-        for requirement in requirements:
-            requirement.installed = is_package_installed(requirement.name)
-
-    context = {
-        'requirements': requirements
-    }
-    return render(request, 'requirements.html', context)
-
-
-def is_package_installed(package_name):
-    # 패키지 설치 여부 확인
-    return True
-
-
-def install_package(package_name):
-    # 패키지 설치
-    print(f"{package_name} has been installed.")
 
 def copyrights(request):
     return render(request, 'copyrights.html')
