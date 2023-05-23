64fb3a08432ac4b6998956ae37c538c5e9774ffa
M1A_ <ayd1fxmx@gmail.com>
2023-05-23 21:04:35 +0900

    .

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Socom_TeamProject
+
+Django 환경 구성에 따른 변경
+HTML -> templates
+Python -> Socom_TeamProject
---
+++ Socom_TeamProject/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+
+def stock(request):
+    return render(request, 'stock.html')
+
+def prediction(request):
+    return render(request, 'prediction.html')
+
+def beginner(request):
+    return render(request, 'beginner.html')
+
+def requirements(request):
+    return render(request, 'requirements.html')
+
+def copyrights(request):
+    return render(request, 'copyrights.html')
