f6f09f8f26cd5786a9e2c62eaea192e294fddf92
Taeho_Kim <teahotiger@gmail.com>
2023-05-30 21:57:44 +0900

    openai create API keys

---
+++ Test_Python/ChatGPT_Test_API.py
@@ -0,0 +1,10 @@
+# Python >= 3.8 installed
+# Use VSCode
+# pip install openai
+
+# search URL https://platform.openai.com/ and LOGIN, and generate API Keys
+
+import openai
+
+openai.api_key = ""
+# Copy your API Keys in ""
