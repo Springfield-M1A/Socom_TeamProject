3c608ccbe8f2054ebf210af2abe3cb551725bf84
Taeho_Kim <teahotiger@gmail.com>
2023-05-30 22:05:19 +0900

    insert gpt model

--- Test_Python/ChatGPT_Test_API.py
+++ Test_Python/ChatGPT_Test_API.py
@@ -8,3 +8,14 @@ import openai
 
 openai.api_key = ""
 # Copy your API Keys in ""
+
+completion = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    massages=[
+        {
+            "role": "user",
+            "content": "Hi? Can you hear me?",
+        }
+    ],
+)
+print(completion)
