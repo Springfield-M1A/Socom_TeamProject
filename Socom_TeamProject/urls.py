dd933eda9e1911efbd62ac555021ab5c22f01bdf
M1A_ <ayd1fxmx@gmail.com>
2023-06-10 03:42:17 +0900

    .

--- Socom_TeamProject/urls.py
+++ Socom_TeamProject/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from .views import index, stock, prediction, beginner, copyrights
 from stockapp import views
+from predictionapp import views
 from . import views
 
 urlpatterns = [
     path('', index, name='index'),
     path('stock.html', stock, name='stock'),
-    path('prediction.html', prediction, name='prediction'),
+    path('predictionapp.url', prediction, name='prediction'),
     path('beginner.html', beginner, name='beginner'),
     path('copyrights.html', copyrights, name='copyrights'),
-    path('', views.prediction, name='stock_prediction'),
 ]
